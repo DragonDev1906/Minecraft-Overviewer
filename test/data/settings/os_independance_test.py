5d15a32f79029e42f535d9a0939fd1da02e3752b
DragonDev1906 <8270201+DragonDev1906@users.noreply.github.com>
2020-04-13 15:09:17 +0200

    Create settings for os_independance_test

---
+++ test/data/settings/os_independance_test.py
@@ -0,0 +1,10 @@
+worlds['test'] = "test/data/worlds/example"
+
+renders["myworld"] = { 
+    "title": "myworld title",
+    "world": "test",
+    "rendermode": normal,
+    "northdirection": "upper-left",
+}
+
+outputdir = "/tmp/fictional/outputdir"
